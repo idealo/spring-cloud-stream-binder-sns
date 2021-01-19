f88cba1c6beee7cbbedbfbbdc5116388c48e2e5e
heiko.rothe <heiko.rothe@idealo.de>
2021-01-19 15:28:14 +0100

    fix: add proper SNS client config

--- src/main/java/de/idealo/spring/stream/binder/sns/config/SnsBinderConfiguration.java
+++ src/main/java/de/idealo/spring/stream/binder/sns/config/SnsBinderConfiguration.java
@@ -2,12 +2,15 @@ package de.idealo.spring.stream.binder.sns.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.aws.core.env.ResourceIdResolver;
+import org.springframework.cloud.aws.core.region.RegionProvider;
 import org.springframework.cloud.stream.binder.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSAsync;
+import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder;
 
 import de.idealo.spring.stream.binder.sns.SnsMessageHandlerBinder;
 import de.idealo.spring.stream.binder.sns.provisioning.SnsStreamProvisioner;
@@ -15,6 +18,15 @@ import de.idealo.spring.stream.binder.sns.provisioning.SnsStreamProvisioner;
 @Configuration
 @ConditionalOnMissingBean(Binder.class)
 public class SnsBinderConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(AmazonSNSAsync.class)
+    public AmazonSNSAsync amazonSNS(AWSCredentialsProvider awsCredentialsProvider, RegionProvider regionProvider) {
+        AmazonSNSAsyncClientBuilder builder = AmazonSNSAsyncClientBuilder.standard();
+        builder.setCredentials(awsCredentialsProvider);
+        builder.setRegion(regionProvider.getRegion().getName());
+        return builder.build();
+    }
 
     @Bean
     public SnsStreamProvisioner provisioningProvider(AmazonSNS amazonSNS, ResourceIdResolver resourceIdResolver) {
