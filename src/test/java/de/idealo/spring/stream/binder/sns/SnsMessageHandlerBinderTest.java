857c6b2863842e5ea2038578b443997db15214bf
hendrik.nibbrig <hendrik.nibbrig@idealo.de>
2022-03-01 16:56:49 +0100

    feat: enable synchronous message sending
    Co-authored-by: Jan Kockrow <jan.kockrow@idealo.de>

--- src/test/java/de/idealo/spring/stream/binder/sns/SnsMessageHandlerBinderTest.java
+++ src/test/java/de/idealo/spring/stream/binder/sns/SnsMessageHandlerBinderTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.cloud.stream.binder.ExtendedProducerProperties;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.expression.common.LiteralExpression;
 import org.springframework.integration.aws.outbound.SnsMessageHandler;
@@ -14,6 +15,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import com.amazonaws.services.sns.AmazonSNSAsync;
 
+import de.idealo.spring.stream.binder.sns.properties.SnsProducerProperties;
 import de.idealo.spring.stream.binder.sns.provisioning.SnsProducerDestination;
 
 class SnsMessageHandlerBinderTest {
@@ -21,19 +23,21 @@ class SnsMessageHandlerBinderTest {
     @Test
     void createSnsMessageHandlerWithExpectedProperties() throws Exception {
         AmazonSNSAsync amazonSNS = mock(AmazonSNSAsync.class);
-        SnsMessageHandlerBinder snsMessageHandlerBinder = new SnsMessageHandlerBinder(amazonSNS, null);
+        SnsMessageHandlerBinder sut = new SnsMessageHandlerBinder(amazonSNS, null, null);
 
         GenericApplicationContext applicationContext = new GenericApplicationContext();
-        snsMessageHandlerBinder.setApplicationContext(applicationContext);
+        sut.setApplicationContext(applicationContext);
 
         SnsProducerDestination snsProducerDestination = new SnsProducerDestination("name", "arn");
-        SnsMessageHandler producerMessageHandler = (SnsMessageHandler)snsMessageHandlerBinder.createProducerMessageHandler(snsProducerDestination, null, null);
+        SnsProducerProperties snsProducerProperties = new SnsProducerProperties();
+        snsProducerProperties.setSync(true);
+        SnsMessageHandler producerMessageHandler = (SnsMessageHandler)sut.createProducerMessageHandler(snsProducerDestination, new ExtendedProducerProperties<>(snsProducerProperties), null);
 
         assertThat(producerMessageHandler).isNotNull();
         assertThat((AmazonSNSAsync) ReflectionTestUtils.getField(producerMessageHandler, "amazonSns")).isEqualTo(amazonSNS);
         assertThat(((LiteralExpression) ReflectionTestUtils.getField(producerMessageHandler, "topicArnExpression")).getValue()).isEqualTo("arn");
         assertThat((MessageChannel) ReflectionTestUtils.getField(producerMessageHandler, "failureChannel")).isNull();
         assertThat((BeanFactory) ReflectionTestUtils.getField(producerMessageHandler, "beanFactory")).isEqualTo(applicationContext.getBeanFactory());
-        assertThat((Boolean) ReflectionTestUtils.getField(producerMessageHandler, "sync")).isFalse();
+        assertThat((Boolean) ReflectionTestUtils.getField(producerMessageHandler, "sync")).isTrue();
     }
 }
